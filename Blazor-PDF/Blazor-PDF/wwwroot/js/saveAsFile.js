599f1b33e39b12a58720c7e2f606ed4625f50a7e
Christophe Peugnet <christophe.peugnet@outlook.fr>
2020-04-11 17:40:17 +0200

    creation

---
+++ Blazor-PDF/Blazor-PDF/wwwroot/js/saveAsFile.js
@@ -0,0 +1,8 @@
+﻿function saveAsFile(filename, byteBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + byteBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
