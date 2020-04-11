8e0aba12b65d2d6ea351b9e849b96024a2b69706
Christophe Peugnet <christophe.peugnet@outlook.fr>
2020-04-11 17:56:33 +0200

    creation

---
+++ Blazor-PDF/Blazor-PDF/PDF/report.cs
@@ -0,0 +1,43 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_PDF.PDF
+{
+    public class Report
+    {
+
+        Document _doc;
+        MemoryStream memoryStream = new MemoryStream();
+        Font _fontStyle;
+
+        private byte[] ReportPDF()
+        {
+            _doc = new Document(PageSize.A4, 10f, 10f, 20f, 20f);
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            PdfWriter.GetInstance(_doc, memoryStream);
+            _doc.Open();
+
+            var phrase = new Phrase("Planning", _fontStyle);
+
+            _doc.Add(phrase);
+
+            _doc.Close();
+            return memoryStream.ToArray();
+        }
+
+        public void Generate(IJSRuntime js)
+        {
+            js.InvokeVoidAsync(
+                "saveAsFile",
+                "report.pdf",
+                Convert.ToBase64String(ReportPDF())
+                );
+        }
+    }
+}
--- Blazor-PDF/Blazor-PDF/Startup.cs
+++ Blazor-PDF/Blazor-PDF/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Blazor_PDF.Data;
 
 namespace Blazor_PDF
 {
@@ -28,7 +27,6 @@ namespace Blazor_PDF
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
