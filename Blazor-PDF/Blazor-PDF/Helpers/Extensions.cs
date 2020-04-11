44c42d4ad4a6aaf67e8aa7b819cde71eb78670fc
Christophe Peugnet <christophe.peugnet@outlook.fr>
2020-04-11 18:59:45 +0200

    .

---
+++ Blazor-PDF/Blazor-PDF/Helpers/Extensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor_PDF
+{
+    public static class Extensions
+    {
+        public static float ToDpi(this float centimeter)
+        {
+            var inch = centimeter / 2.54;
+            return (float)(inch * 72);
+        }
+    }
+}
--- Blazor-PDF/Blazor-PDF/PDF/report.cs
+++ Blazor-PDF/Blazor-PDF/PDF/report.cs
@@ -6,30 +6,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazor_PDF;
 
 namespace Blazor_PDF.PDF
 {
     public class Report
     {
 
-        Document _doc;
-        MemoryStream memoryStream = new MemoryStream();
-        Font _fontStyle;
-
-        private byte[] ReportPDF()
-        {
-            _doc = new Document(PageSize.A4, 10f, 10f, 20f, 20f);
-            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
-            PdfWriter.GetInstance(_doc, memoryStream);
-            _doc.Open();
-
-            var phrase = new Phrase("Planning", _fontStyle);
-
-            _doc.Add(phrase);
-
-            _doc.Close();
-            return memoryStream.ToArray();
-        }
+        private Document _docPDF;
+        private PdfWriter writer;
 
         public void Generate(IJSRuntime js)
         {
@@ -38,6 +23,54 @@ namespace Blazor_PDF.PDF
                 "report.pdf",
                 Convert.ToBase64String(ReportPDF())
                 );
+        }
+
+        private byte[] ReportPDF()
+        {
+            var memoryStream = new MemoryStream();
+
+            float margeLeft = 1.5f;
+            float margeRight= 1.5f;
+            float margeTop = 2f;
+            float margeBottom = 2f;
+
+            _docPDF = new Document(
+                            PageSize.A4,
+                            margeLeft.ToDpi(),
+                            margeRight.ToDpi(),
+                            margeTop.ToDpi(),
+                            margeBottom.ToDpi());
+
+            writer = PdfWriter.GetInstance(_docPDF, memoryStream);
+            _docPDF.Open();
+
+            PageText(_docPDF);
+
+            _docPDF.Close();
+            return memoryStream.ToArray();
+        }
+
+
+        private void PageText(Document docPDF)
+        {
+            Font _fontStyle = FontFactory.GetFont("Tahoma", 8f, Font.NORMAL);
+
+            var lopsem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum felis ut turpis viverra, a ultrices nisi tempor. Aliquam suscipit dui sit amet facilisis aliquam. In scelerisque sem ut elit molestie tempor. In finibus sagittis nulla, vitae vestibulum ante tristique sit amet. Phasellus facilisis rhoncus nunc id scelerisque. Praesent cursus erat nec turpis interdum condimentum. Aenean ut facilisis eros. Nam semper tincidunt libero in porttitor. Praesent nec dui vitae leo vulputate varius ut non risus. Quisque imperdiet euismod ipsum facilisis finibus. Duis ac felis eget leo malesuada gravida id at felis. Cras posuere, tortor sit amet bibendum tincidunt, augue lectus pulvinar nisl, ac blandit velit arcu sed nulla. Mauris id venenatis turpis, ut fringilla nunc. Aenean commodo fermentum nulla, non porta sapien viverra sed. Sed sed risus interdum, maximus sapien ac, bibendum diam.";
+
+            var phrase = new Phrase(lopsem, _fontStyle);
+
+
+            //PdfContentByte cb = writer.DirectContent;
+            //cb.SetLineWidth(3f);
+            //cb.MoveTo(100, 700);
+            //cb.LineTo(200, 800);
+
+            //ColumnText ct = new ColumnText( cb);
+            //ct.SetSimpleColumn((new Rectangle(0, 0, 523, 50));
+            //ct.addElement(new Paragraph("This could be a very long sentence that needs to be wrapped"));
+            //ct.go();
+
+            docPDF.Add(phrase);
         }
     }
 }
