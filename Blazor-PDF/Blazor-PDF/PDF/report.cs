a244b1c5d82fd1d3db5795be7c6bd66b8ed13e8f
Christophe Peugnet <christophe.peugnet@outlook.fr>
2020-04-13 11:52:54 +0200

    add page bookmarks

--- Blazor-PDF/Blazor-PDF/PDF/report.cs
+++ Blazor-PDF/Blazor-PDF/PDF/report.cs
@@ -15,9 +15,12 @@ namespace Blazor_PDF.PDF
 
         private Document _docPDF;
         private PdfWriter writer;
+        private int _pagenumber;
+        private readonly string _lopsem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum felis ut turpis viverra, a ultrices nisi tempor. Aliquam suscipit dui sit amet facilisis aliquam. In scelerisque sem ut elit molestie tempor. In finibus sagittis nulla, vitae vestibulum ante tristique sit amet. Phasellus facilisis rhoncus nunc id scelerisque. Praesent cursus erat nec turpis interdum condimentum. Aenean ut facilisis eros. Nam semper tincidunt libero in porttitor. Praesent nec dui vitae leo vulputate varius ut non risus. Quisque imperdiet euismod ipsum facilisis finibus. Duis ac felis eget leo malesuada gravida id at felis. Cras posuere, tortor sit amet bibendum tincidunt, augue lectus pulvinar nisl, ac blandit velit arcu sed nulla. Mauris id venenatis turpis, ut fringilla nunc. Aenean commodo fermentum nulla, non porta sapien viverra sed. Sed sed risus interdum, maximus sapien ac, bibendum diam.";
 
-        public void Generate(IJSRuntime js)
+        public void Generate(IJSRuntime js, int pagenumber)
         {
+            _pagenumber = pagenumber;
             js.InvokeVoidAsync(
                 "saveAsFile",
                 "report.pdf",
@@ -41,36 +44,45 @@ namespace Blazor_PDF.PDF
                             margeTop.ToDpi(),
                             margeBottom.ToDpi());
 
+            _docPDF.AddTitle("Blazor-PDF");
+            _docPDF.AddAuthor( "Christophe Peugnet");
+            _docPDF.AddCreationDate();
+            _docPDF.AddKeywords("blazor");
+            _docPDF.AddSubject("Create a pdf file with iText");
+
             writer = PdfWriter.GetInstance(_docPDF, memoryStream);
+
             _docPDF.Open();
 
-            PageText(_docPDF);
+            if ( _pagenumber == 1 )
+                PageText();
+            else if ( _pagenumber == 2 )
+                PageBookmark();
 
             _docPDF.Close();
+
             return memoryStream.ToArray();
         }
 
 
-        private void PageText(Document docPDF)
+        private void PageText()
         {
             Font _fontStyle = FontFactory.GetFont("Tahoma", 8f, Font.NORMAL);
 
-            var lopsem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum felis ut turpis viverra, a ultrices nisi tempor. Aliquam suscipit dui sit amet facilisis aliquam. In scelerisque sem ut elit molestie tempor. In finibus sagittis nulla, vitae vestibulum ante tristique sit amet. Phasellus facilisis rhoncus nunc id scelerisque. Praesent cursus erat nec turpis interdum condimentum. Aenean ut facilisis eros. Nam semper tincidunt libero in porttitor. Praesent nec dui vitae leo vulputate varius ut non risus. Quisque imperdiet euismod ipsum facilisis finibus. Duis ac felis eget leo malesuada gravida id at felis. Cras posuere, tortor sit amet bibendum tincidunt, augue lectus pulvinar nisl, ac blandit velit arcu sed nulla. Mauris id venenatis turpis, ut fringilla nunc. Aenean commodo fermentum nulla, non porta sapien viverra sed. Sed sed risus interdum, maximus sapien ac, bibendum diam.";
-
-            var phrase = new Phrase(lopsem, _fontStyle);
+            var phrase = new Phrase(_lopsem, _fontStyle);
 
             // Create and add a Paragraph
             Paragraph p = new Paragraph("Paragraph On the Right", _fontStyle);
             p.SetAlignment("RIGHT");
-            docPDF.Add(p);
+            _docPDF.Add(p);
 
             // Create and add an Image
             string image = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\images\Logo.png"}";
             Image img = Image.GetInstance(image);
-            //img.SetAbsolutePosition(
-            //        (PageSize.POSTCARD.getWidth() - img.getScaledWidth()) / 2,
-            //        (PageSize.POSTCARD.getHeight() - img.getScaledHeight()) / 2);
-            docPDF.Add(img);
+            img.SetAbsolutePosition(
+                    (PageSize.A4.Width - img.ScaledWidth) / 2,
+                    (PageSize.A4.Height - img.ScaledHeight) / 2);
+            _docPDF.Add(img);
 
 
             PdfContentByte cb = writer.DirectContent;
@@ -85,7 +97,43 @@ namespace Blazor_PDF.PDF
             
             ct.Go();
 
-            docPDF.Add(phrase);
+            _docPDF.Add(phrase);
         }
+
+        private void PageBookmark()
+        {
+            float indentation = 20;
+
+            Chapter chapter1 = new Chapter(new Paragraph("Text and Paragraphe"), 1)
+            {
+                BookmarkTitle = "Text & co",
+                BookmarkOpen = true
+            };
+
+            Section section1 = chapter1.AddSection(indentation, "Section 1.1", 2);
+            Section section2 = chapter1.AddSection(indentation, "Section 1.2", 2);
+
+            Section subsection1 = section2.AddSection(indentation, "Subsection 1.2.1", 3);
+            Section subsection2 = section2.AddSection(20f, "Subsection 1.2.2", 3);
+
+            Section subsubsection = subsection2.AddSection(indentation, "Sub Subsection 1.2.2.1", 4);
+
+            Chapter chapter2 = new Chapter(new Paragraph("This is Chapter 2"),2)
+            {
+                BookmarkOpen = false,
+                TriggerNewPage=false
+            };
+          
+            Section section3 = chapter2.AddSection("Section 2.1", 3);
+
+            Section subsection3 = section3.AddSection("Subsection 2.1.1", 4);
+
+            Section section4 = chapter2.AddSection("Section 2.2", 3);
+
+            _docPDF.Add(chapter1);
+
+            _docPDF.Add(chapter2);
+        }
+
     }
 }
